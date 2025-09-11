9de1440ea2cae762f3ded65fe307e0985d3ef592
Jason Santiago aka Cyber-Associate | Lifetime Learner <122810889+jsanti1975on@users.noreply.github.com>
2025-09-10 20:37:35 -0400

    Delete Cyber_Range directory

--- Cyber_Range/cyber-lab-dashboard-launcher/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd "$(dirname "$0")/src"
-
-x86_64-w64-mingw32-windres icon.rc -O coff -o icon.res
-x86_64-w64-mingw32-gcc open_dash.c icon.res -o ../open-dash.exe -mwindows -s
-
-echo "[+] Built $(cd .. && pwd)/open-dash.exe"
--- Cyber_Range/images/README.md
+++
@@ -1,4 +0,0 @@
-# This will be the images directory
-
-<img width="1920" height="1080" alt="072625-2nd-dash-image" src="https://github.com/user-attachments/assets/4a41b14b-3122-49cb-81eb-b0940dbcd45f" />
-
